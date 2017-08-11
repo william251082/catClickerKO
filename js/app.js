4766814b2e3eb666c626fbea22b6773965e0a35b
William del Rosario <williamdelrosario@yahoo.com>
2017-08-11 12:05:18 +0200

    added the data parameter to Cat function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
-	this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T', 'Tab']);
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 
 	this.title = ko.computed(function(){
 		var title;
@@ -29,7 +29,13 @@ var Cat = function() {
 var ViewModel = function(){
 	var self = this;
 
-	this.currentCat = ko.observable(new Cat());
+	this.currentCat = ko.observable(new Cat({
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+		nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tab']
+	}));
 	
 	this. incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
