c20e30eacd5b0f6c653664e3aeb35dedd9fa3a8d
William del Rosario <williamdelrosario@yahoo.com>
2017-08-11 11:11:37 +0200

    Computed Observables & Cat function

--- js/app.js
+++ js/app.js
@@ -1,40 +1,38 @@
-var ViewModel = function(){
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
+	this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T', 'Tab']);
 
-	this. incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-
-	this.catLevel = ko.computed(function() {
-		var clicks = this.incrementCounter();
-		if (clicks <= 10) {
-      return 'Newborn';
-    } else if (clicks >= 40) {
-      return 'Infant';
-    } else {
-      return 'Teen';
-    }
-  }, this);
+	this.title = ko.computed(function(){
+		var title;
+		var clicks = this.clickCount();
+		if (clicks < 10) {
+			title = 'Newborn';
+		} else if (clicks < 50) {
+			title = 'Infant';
+		} else if (clicks < 100) {
+			title = 'Child';
+		} else if (clicks < 200) {
+			title = 'Teen';
+		} else if (clicks < 500) {
+			title = 'Adult';
+		} else { 
+			title = 'Ninja';
+		}
+		return title;
+	}, this);
 }
 
-	/* means
-		var count = 0;
-		count++;
-		*/
+
+var ViewModel = function(){
+
+	this.currentCat = ko.observable(new Cat());
+	
+	this. incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		};
 	};
 
-}
-
 ko.applyBindings(new ViewModel());
-
-/*
-function AppViewModel() {
-    // ... leave firstName and lastName unchanged ...
- 
-    this.fullName = ko.computed(function() {
-        return this.firstName() + " " + this.lastName();
-    }, this);
-} */
-
